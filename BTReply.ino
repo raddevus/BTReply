8bfb034e6328729647f3a8aa253da883c0c69d06
raddevus <1radeutsch1@gmail.com>
2021-06-01 10:44:29 -0400

    Initial creation of very basic project used to test connecting to hc-05 BT devices -- and get bytes returned.

---
+++ BTReply.ino
@@ -0,0 +1,63 @@
+/*
+  Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 2 (connect to TX of other device)
+ * TX is digital pin 3 (connect to RX of other device)
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial SW_Serial(9, 10); // RX, TX
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Native USB only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  SW_Serial.begin(38400);
+  SW_Serial.println("Hello, world?");
+}
+
+void loop() // run over and over
+{
+  String command = "";
+  while (SW_Serial.available()){
+    command.concat((char)SW_Serial.read());
+  }
+  if (command != ""){
+    Serial.println(command);
+  }
+  if (command == "getTemp"){
+      SW_Serial.println("79.9");
+  }
+  //Serial.write(SW_Serial.read());
+  if (Serial.available())
+    SW_Serial.write(Serial.read());
+}
