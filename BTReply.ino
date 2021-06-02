b63ce917f7ae89e4a37a390d6be4a7392a3a7198
raddevus <1radeutsch1@gmail.com>
2021-06-02 16:53:58 -0400

    Made major changes so this sketch can be used with short commands (1 byte) and easily used with SoBtEx (android app - available via Github).  1- returns basic msg 2- returns random value 3- returns file data (if sd card reader is avail and file exists) 4- creates/appends 3 comma-delimited random values to lines of test.txt file. All these can be used together to test BT replies.

--- BTReply.ino
+++ BTReply.ino
@@ -1,36 +1,20 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 2 (connect to TX of other device)
- * TX is digital pin 3 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <SoftwareSerial.h>
+#include <SD.h>
 
-SoftwareSerial SW_Serial(9, 10); // RX, TX
+const int CS_PIN = 10;
+bool isSDCardInitialized = false;
+
+SoftwareSerial SW_Serial(8,9); // RX, TX
+
+// we will always consider a zero as no command
+byte command = 0;
 
 void setup()
 {
+  // Prepping SD Card component
+  //CS pin must be configured as an output
+  pinMode(CS_PIN, OUTPUT);
+  initSDCard();
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
@@ -38,26 +22,95 @@ void setup()
   }
 
 
-  Serial.println("Goodnight moon!");
+  Serial.println("Ready for output...");
 
   // set the data rate for the SoftwareSerial port
   SW_Serial.begin(38400);
   SW_Serial.println("Hello, world?");
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
 }
 
-void loop() // run over and over
+void loop()
 {
-  String command = "";
+  // always initialize the command back to 0
+  command = 0;  
   while (SW_Serial.available()){
-    command.concat((char)SW_Serial.read());
+    command = SW_Serial.read();
   }
-  if (command != ""){
-    Serial.println(command);
+  switch (command){
+    case 49: { // ASCII char 1
+      SW_Serial.println("This is a basic message sent over BT.");
+      break;
+    }
+    case 50: { // ASCII char 2
+      SW_Serial.println(random(50000));
+      break;
+    }
+    case 51: { // ASCII char 3
+      if (isSDCardInitialized){
+        String output = "";
+        SW_Serial.print("Retrieving file...\n");
+        File dataFile = SD.open("test.txt", FILE_READ);
+        if (dataFile) { 
+        while (dataFile.available()) { //execute while file is available
+          char letter = dataFile.read(); //read next character from file
+          // I strip off the 13 found on each line of the 
+          // text file, then I put just one back on (below 
+          // in the SW_Serial.prinln(output) so that the SoBtEx Android
+          // app will recognize the end of transmission. It's odd
+          // but it works.
+          if (letter != 13){
+            output.concat(letter);
+          }
+        }
+        SW_Serial.println(output); //display all
+        //SW_Serial.println("");
+        dataFile.close(); //close file
+        }
+      }
+      else{
+        SW_Serial.println("There is no SD Card in the device.\nOr the device is not available.");
+      }
+      break;
+    }
+    case 52: { // ASCII char 4
+      File dataFile = SD.open("test.txt", FILE_WRITE);
+       if (dataFile)
+       {
+        dataFile.print(random(50000));
+        dataFile.print(",");
+        dataFile.print(random(50000));
+        dataFile.print(",");
+        dataFile.println(random(50000));
+        dataFile.close(); //Data isn't written until we run close()!
+        SW_Serial.println("Writing some data to the file: test.txt");
+       }
+      
+      break;
+    }
   }
-  if (command == "getTemp"){
-      SW_Serial.println("79.9");
+  if (command != 0){
+    Serial.println("got : " + (char)command);
   }
+
+  
   //Serial.write(SW_Serial.read());
   if (Serial.available())
     SW_Serial.write(Serial.read());
+}
+
+void initSDCard(){
+ if (isSDCardInitialized){
+  return;
+ }
+ 
+ if (SD.begin(CS_PIN))
+ {
+   isSDCardInitialized = true;
+ }
 }
